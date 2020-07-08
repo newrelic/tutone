66f074dfed72b50abd8951c5205bd446cc4e683d
Zach Leslie <zleslie@newrelic.com>
2020-07-08 15:17:59 -0700

    chore: move generate/fetch packages to pkg/

--- pkg/generate/command.go
+++ pkg/generate/command.go
@@ -5,8 +5,8 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	"github.com/newrelic/tutone/internal/fetch"
 	"github.com/newrelic/tutone/internal/util"
+	"github.com/newrelic/tutone/pkg/fetch"
 )
 
 const (
