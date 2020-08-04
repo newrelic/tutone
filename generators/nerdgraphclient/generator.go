1d2b6573a3a97a8d947c7d626b1ce3554420d4e0
Zach Leslie <zleslie@newrelic.com>
2020-08-03 18:20:35 -0700

    chore(codegen): start another generator for clients

---
+++ generators/nerdgraphclient/generator.go
@@ -0,0 +1,130 @@
+package nerdgraphclient
+
+import (
+	"fmt"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/newrelic/tutone/internal/codegen"
+	"github.com/newrelic/tutone/internal/config"
+	"github.com/newrelic/tutone/internal/schema"
+	"github.com/newrelic/tutone/pkg/lang"
+)
+
+type Generator struct {
+	lang.GolangGenerator
+}
+
+func (g *Generator) Generate(s *schema.Schema, genConfig *config.GeneratorConfig, pkgConfig *config.PackageConfig) error {
+	if genConfig == nil {
+		return fmt.Errorf("unable to Generate with nil genConfig")
+	}
+
+	if pkgConfig == nil {
+		return fmt.Errorf("unable to Generate with nil pkgConfig")
+	}
+
+	expandedTypes, err := schema.ExpandTypes(s, pkgConfig.Types, pkgConfig.Methods)
+	if err != nil {
+		log.Error(err)
+	}
+
+	structsForGen, enumsForGen, scalarsForGen, interfacesForGen, err := lang.GenerateGoTypesForPackage(s, genConfig, pkgConfig, expandedTypes)
+	if err != nil {
+		return err
+	}
+
+	// TODO idea:
+	// err = lang.GenerateTypesForPackage(&g)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// lang.Normalize(&g, genConfig, pkgConfig)
+
+	g.PackageName = pkgConfig.Name
+	g.Imports = pkgConfig.Imports
+
+	if structsForGen != nil {
+		g.Types = *structsForGen
+	}
+
+	if enumsForGen != nil {
+		g.Enums = *enumsForGen
+	}
+
+	if scalarsForGen != nil {
+		g.Scalars = *scalarsForGen
+	}
+
+	if interfacesForGen != nil {
+		g.Interfaces = *interfacesForGen
+	}
+
+	methodsForGen, err := lang.GenerateGoMethodsForPackage(s, genConfig, pkgConfig)
+	if err != nil {
+		return err
+	}
+
+	if methodsForGen != nil {
+		g.Methods = *methodsForGen
+	}
+
+	return nil
+}
+
+func (g *Generator) Execute(genConfig *config.GeneratorConfig, pkgConfig *config.PackageConfig) error {
+	var err error
+
+	// Default to project root for types
+	destinationPath := "./"
+	if pkgConfig.Path != "" {
+		destinationPath = pkgConfig.Path
+	}
+
+	if _, err = os.Stat(destinationPath); os.IsNotExist(err) {
+		if err = os.Mkdir(destinationPath, 0755); err != nil {
+			log.Error(err)
+		}
+	}
+
+	// Default file name is 'nerdgraph.go'
+	fileName := "nerdgraphclient.go"
+	if genConfig.FileName != "" {
+		fileName = genConfig.FileName
+		if err != nil {
+			return err
+		}
+	}
+
+	templateName := "client.go.tmpl"
+	if genConfig.TemplateName != "" {
+		templateName = genConfig.TemplateName
+		if err != nil {
+			return err
+		}
+	}
+
+	filePath, err := codegen.RenderStringFromGenerator(fmt.Sprintf("%s/%s", destinationPath, fileName), g)
+	if err != nil {
+		return err
+	}
+
+	templateDir := "templates/nerdgraphclient"
+	if genConfig.TemplateDir != "" {
+		templateDir, err = codegen.RenderStringFromGenerator(genConfig.TemplateDir, g)
+		if err != nil {
+			return err
+		}
+	}
+
+	c := codegen.CodeGen{
+		TemplateDir:     templateDir,
+		TemplateName:    templateName,
+		DestinationFile: filePath,
+		DestinationDir:  destinationPath,
+	}
+
+	return c.WriteFile(g)
+}
